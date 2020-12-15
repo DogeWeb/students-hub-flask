24e98518069e285a731908829b3cdae9c57f74cf
dogeweb <simonex720@gmail.com>
2020-12-15 17:40:25 +0100

    Added Team page

--- main/views.py
+++ main/views.py
@@ -3,10 +3,14 @@ from flask import Blueprint
 from flask_login import current_user
 from flask_login import login_required
 
-
 main_blueprint = Blueprint('main', __name__, )
 
 
 @main_blueprint.route('/')
 def home():
     return render_template('main/index.html', current_user=current_user)
+
+
+@main_blueprint.route('/team')
+def team():
+    return render_template('main/team.html', current_user=current_user)
